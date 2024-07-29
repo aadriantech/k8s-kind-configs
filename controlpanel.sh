312ec53d7fd244ce782de8285f364890f48abf8f
adrian <adrian.adriano.tech@gmail.com>
2024-07-29 11:52:20 -0400

    feat(controlpanel): add names to containers

--- controlpanel.sh
+++ controlpanel.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-capstone_containers=(
-    "0ea25d0c23412534359ad34dbaeab38c2016d3b7cf8d8f9e58fae9cb4d93bcce"
-    "a5ab71b3046ea733f549417e36a8fc4b0420653c24bd41734d2a5703dadc9619"
-    "a7a8d744dce5f49671c3b64e09f1ccd78b9085e509f191c04c61ac8524228c80"
+declare -A capstone_containers=(
+    ["capstone-worker"]="0ea25d0c23412534359ad34dbaeab38c2016d3b7cf8d8f9e58fae9cb4d93bcce"
+    ["capstone-control-plane"]="a5ab71b3046ea733f549417e36a8fc4b0420653c24bd41734d2a5703dadc9619"
+    ["capstone-worker2"]="a7a8d744dce5f49671c3b64e09f1ccd78b9085e509f191c04c61ac8524228c80"
 )
 
-sandbox_containers=(
-    "a20c11cb07fa63d43d0dbb7b01c035906e8649b6efa671776c28776630de4164"
-    "fe987edfdab94a6c25a2761590747e9dc107c6f6f2afb8bc4a9c8bc43b719c8c"
-    "20ae925560fd36ddc71b53c04c5eff18bb62508b627524989eda03abc68dccef"
+declare -A sandbox_containers=(
+    ["sandbox-worker"]="a20c11cb07fa63d43d0dbb7b01c035906e8649b6efa671776c28776630de4164"
+    ["sandbox-control-plane"]="fe987edfdab94a6c25a2761590747e9dc107c6f6f2afb8bc4a9c8bc43b719c8c"
+    ["sandbox-worker2"]="20ae925560fd36ddc71b53c04c5eff18bb62508b627524989eda03abc68dccef"
 )
 
 manage_containers() {
     local action=$1
     shift
-    local containers=("$@")
-    for container in "${containers[@]}"; do
-        echo "${action^} container: $container"
-        docker "$action" "$container"
+    local -n containers=$1
+    for name in "${!containers[@]}"; do
+        container_id=${containers[$name]}
+        echo "${action^}ing container: $name (ID: $container_id)"
+        docker "$action" "$container_id"
     done
 }
 
@@ -49,15 +50,16 @@ read -p "Enter your choice (1/2/3): " cluster_choice
 case $cluster_choice in
     1)
         echo "${action^}ing Capstone cluster containers..."
-        manage_containers "$action" "${capstone_containers[@]}"
+        manage_containers "$action" capstone_containers
         ;;
     2)
         echo "${action^}ing Sandbox cluster containers..."
-        manage_containers "$action" "${sandbox_containers[@]}"
+        manage_containers "$action" sandbox_containers
         ;;
     3)
         echo "${action^}ing both Capstone and Sandbox cluster containers..."
-        manage_containers "$action" "${capstone_containers[@]}" "${sandbox_containers[@]}"
+        manage_containers "$action" capstone_containers
+        manage_containers "$action" sandbox_containers
         ;;
     *)
         echo "Invalid choice. Exiting."
@@ -66,4 +68,3 @@ case $cluster_choice in
 esac
 
 echo "Container ${action} process completed."
-
